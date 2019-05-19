370a14d26530ddad533a647fa0f16203c49787db
Marc <mfmunic@gmail.com>
2019-05-19 16:07:58 -0700

    moved

--- src/components/utils/Logo.js
+++ src/components/utils/Logo.js
@@ -1,23 +1,22 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import * as display from "../../modules/actions/display";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as display from '../../modules/actions/display';
 
 class Logo extends Component {
   goHome() {
-    this.props.dispatch(display.menu("Homepage", "blue"));
+    this.props.dispatch(display.menu('Homepage', 'blue'));
   }
 
   render() {
     const { color } = this.props.display;
     return (
-      <div id="templogo">
+      <div className="logo-border z8">
         <div
-          id="nameLogo"
-          className={`${color}-BG`}
+          className={`logo BG--${color} z10`}
           onClick={this.goHome.bind(this)}
         >
-          <h1>Marc</h1>
-          <h1>Munic</h1>
+          <h1 className="logo-name logo-name-Marc">Marc</h1>
+          <h1 className="logo-name logo-name-Munic">Munic</h1>
         </div>
       </div>
     );
