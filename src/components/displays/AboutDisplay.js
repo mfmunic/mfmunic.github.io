27b927738df1613e56b8217ee24564a969150c67
marc <mfmunic@gmail.com>
2020-06-08 13:53:37 -0700

    updated 2

--- src/components/displays/AboutDisplay.js
+++ src/components/displays/AboutDisplay.js
@@ -1,23 +1,24 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import _ from "lodash";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import _ from 'lodash';
 
-import AboutText from "./texts/AboutText";
+import AboutText from './texts/AboutText';
 
 class AboutDisplay extends Component {
   render() {
     const aboutWithBreaks = [];
-    _.forEach(AboutText, (about, index) => {
+    // eslint-disable-next-line array-callback-return
+    AboutText.map((about, index) => {
       aboutWithBreaks.push(<p key={index}>{about}</p>);
     });
-    return <div id="aboutDisplay">{aboutWithBreaks}</div>;
+    return <div id='aboutDisplay'>{aboutWithBreaks}</div>;
   }
 }
 
 function mapStateToProps(state) {
   return {
     routing: state.routing,
-    display: state.display
+    display: state.display,
   };
 }
 
--- src/store.js
+++ src/store.js
@@ -1,26 +1,23 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import promiseMiddleware from "redux-promise-middleware";
-import createHistory from "history/createBrowserHistory";
+import { createStore, applyMiddleware, compose } from 'redux';
+import promiseMiddleware from 'redux-promise-middleware';
+import { createBrowserHistory } from 'history';
 
-import rootReducer from "./modules/rootReducer";
+import rootReducer from './modules/rootReducer';
 
-export const history = createHistory();
+export const history = createBrowserHistory();
 
 const initialState = {};
 const enhancers = [];
-const middleware = [promiseMiddleware()];
-if (process.env.NODE_ENV === "development") {
+const middleware = [promiseMiddleware];
+if (process.env.NODE_ENV === 'development') {
   const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
 
-  if (typeof devToolsExtension === "function") {
+  if (typeof devToolsExtension === 'function') {
     enhancers.push(devToolsExtension());
   }
 }
 
-const composedEnhancers = compose(
-  applyMiddleware(...middleware),
-  ...enhancers
-);
+const composedEnhancers = compose(applyMiddleware(...middleware), ...enhancers);
 
 const store = createStore(rootReducer, initialState, composedEnhancers);
 
