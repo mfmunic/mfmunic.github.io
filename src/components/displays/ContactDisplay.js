68c6a44eccf6dcc356076977a2fc81ff05141ef0
marc <mfmunic@gmail.com>
2020-06-15 14:08:30 -0700

    updated resume again

--- src/components/displays/ContactDisplay.js
+++ src/components/displays/ContactDisplay.js
@@ -1,13 +1,13 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-import EmailCI from "./contactItems/EmailCI";
-import LinksCI from "./contactItems/LinksCI";
+import EmailCI from './contactItems/EmailCI';
+import LinksCI from './contactItems/LinksCI';
 
 class ContactDisplay extends Component {
   decidedDisplay(chosen) {
     switch (chosen) {
-      case "Links":
+      case 'Links':
         return <LinksCI />;
       default:
         return <EmailCI />;
@@ -15,15 +15,14 @@ class ContactDisplay extends Component {
   }
 
   render() {
-    const displayItem = this.decidedDisplay(this.props.display.toolbar);
-    return <div>{displayItem}</div>;
+    return this.decidedDisplay(this.props.display.toolbar);
   }
 }
 
 function mapStateToProps(state) {
   return {
     routing: state.routing,
-    display: state.display
+    display: state.display,
   };
 }
 
